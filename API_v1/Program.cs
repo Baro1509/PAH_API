05803eb7a2888cd78a3868f095719448dacc8f29
hoang bao <baohoang.7882@hotmail.com>
2023-09-28 15:17:03 +0700

    feat: add login and register API

--- API_v1/Program.cs
+++ API_v1/Program.cs
@@ -1,14 +1,24 @@
 using API.ErrorHandling;
+using DataAccess;
+using DataAccess.Implement;
+using DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using Service;
+using Service.Implement;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<PlatformAntiquesHandicraftsContext>(options => options.UseSqlServer("name=ConnectionStrings:dev"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<IUserDAO, UserDAO>();
+builder.Services.AddScoped<IUserService, UserService>();
 var app = builder.Build();
 var logger = app.Services.GetRequiredService<ILogger<Program>>();
 
