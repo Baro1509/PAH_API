6ba6c02f9dd67b631bb99da09e4c8c822c4a7e6e
hoang bao <baohoang.7882@hotmail.com>
2023-09-29 15:13:53 +0700

    feat: update image uploading function to service

--- API_v1/Controllers/ImageController.cs
+++ API_v1/Controllers/ImageController.cs
@@ -2,14 +2,17 @@
 using Firebase.Storage;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Service;
 
 namespace API.Controllers {
     [Route("api/[controller]")]
     [ApiController]
     public class ImageController : ControllerBase {
         private readonly IConfiguration _configuration;
-        public ImageController(IConfiguration configuration) {
+        private readonly IImageService _imageService;
+        public ImageController(IConfiguration configuration, IImageService imageService) {
             _configuration = configuration;
+            _imageService = imageService;
         }
 
         [HttpPost]
@@ -21,29 +24,7 @@ namespace API.Controllers {
             // Get any Stream - it can be FileStream, MemoryStream or any other type of Stream
             var stream = file.OpenReadStream();
 
-            //authentication
-            var auth = new FirebaseAuthProvider(new FirebaseConfig(_configuration["Firebase:ApiKey"]));
-            var a = await auth.SignInWithEmailAndPasswordAsync(_configuration["Firebase:AuthEmail"], _configuration["Firebase:AuthPassword"]);
-
-            // Constructr FirebaseStorage, path to where you want to upload the file and Put it there
-            var task = new FirebaseStorage(
-                _configuration["Firebase:Bucket"],
-
-                 new FirebaseStorageOptions {
-                     AuthTokenAsyncFactory = () => Task.FromResult(a.FirebaseToken),
-                     ThrowOnCancel = true,
-                 })
-                .Child("img")
-                .Child("avt")
-                .Child(file.FileName)
-                .PutAsync(stream);
-
-            // Track progress of the upload
-            task.Progress.ProgressChanged += (s, e) => Console.WriteLine($"Progress: {e.Percentage} %");
-
-            // await the task to wait until upload completes and get the download url
-            var downloadUrl = await task;
-            return Ok(new { downloadUrl });
+            return Ok(new { url =  _imageService.StoreImageAsync(file.FileName, stream).Result});
         }
     }
 }
