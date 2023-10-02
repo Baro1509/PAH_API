9108eeb3ea20dff9f37c728a9c04eaf86aed2aaf
hoang bao <baohoang.7882@hotmail.com>
2023-10-02 14:34:17 +0700

    feat: add base order service

---
+++ Service/IOrderService.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service {
+    public interface IOrderService {
+        public void Create(Order order);
+        public Order UpdateOrderStatus(int sellerId, int status, int orderId);
+        public void CancelOrderRequest(int buyerId, int orderId);
+        public void ApproveCancelOrderRequest(int sellerId, int orderId);
+
+        public List<Order> GetByBuyerId(int buyerId);
+        public List<Order> GetBySellerId(int sellerId);
+        public List<Order> GetAll();
+    }
+}
---
+++ Service/Implement/OrderService.cs
@@ -0,0 +1,45 @@
+﻿using DataAccess;
+using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Implement {
+    public class OrderService : IOrderService {
+        private readonly IOrderDAO _orderDAO;
+
+        public OrderService(IOrderDAO orderDAO) {
+            _orderDAO = orderDAO;
+        }
+
+        public void ApproveCancelOrderRequest(int sellerId, int orderId) {
+            throw new NotImplementedException();
+        }
+
+        public void CancelOrderRequest(int buyerId, int orderId) {
+            throw new NotImplementedException();
+        }
+
+        public void Create(Order order) {
+            throw new NotImplementedException();
+        }
+
+        public List<Order> GetAll() {
+            throw new NotImplementedException();
+        }
+
+        public List<Order> GetByBuyerId(int buyerId) {
+            throw new NotImplementedException();
+        }
+
+        public List<Order> GetBySellerId(int sellerId) {
+            throw new NotImplementedException();
+        }
+
+        public Order UpdateOrderStatus(int sellerId, int status, int orderId) {
+            throw new NotImplementedException();
+        }
+    }
+}
