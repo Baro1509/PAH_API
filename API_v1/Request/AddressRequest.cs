80a13b91bc4d2e93886cd45b8b66fabf8a281f1d
hoang bao <baohoang.7882@hotmail.com>
2023-10-05 15:21:20 +0700

    feat: add address request

--- API_v1/Request/AddressRequest.cs
+++ API_v1/Request/AddressRequest.cs
@@ -2,21 +2,23 @@
 
 namespace API.Request {
     public class AddressRequest {
+        public int Id { get; set; }
         [Required]
-        public string? RecipientName { get; set; }
+        public string RecipientName { get; set; }
         [Required]
-        public string? RecipientPhone { get; set; }
-        public string? Province { get; set; }
+        public string RecipientPhone { get; set; }
         [Required]
-        public string? District { get; set; }
+        public string Province { get; set; }
         [Required]
-        public int? DistrictId { get; set; }
+        public string District { get; set; }
         [Required]
-        public string? Ward { get; set; }
+        public int DistrictId { get; set; }
         [Required]
-        public string? WardCode { get; set; }
+        public string Ward { get; set; }
         [Required]
-        public string? Street { get; set; }
+        public string WardCode { get; set; }
+        [Required]
+        public string Street { get; set; }
         [Required]
         public int Type { get; set; }
         public bool? IsDefault { get; set; }
