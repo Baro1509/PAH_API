6bf22048a43e6e454c74f8d3172053d8e878ad74
hoang bao <baohoang.7882@hotmail.com>
2023-10-05 13:58:50 +0700

    update: add remove function

--- Service/IAddressService.cs
+++ Service/IAddressService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataAccess.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +7,10 @@ using System.Threading.Tasks;
 
 namespace Service {
     public interface IAddressService {
+        public List<Address> GetByCustomerId(int customerId);
+        public Address Get(int addressId);
+        public void Create(Address address);
+        public void Update(Address address);
+        public void Delete(int addressId);
     }
 }
--- Service/Implement/AddressService.cs
+++ Service/Implement/AddressService.cs
@@ -4,13 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
+using DataAccess;
+using DataAccess.Models;
 
 namespace Service.Implement {
     public class AddressService : IAddressService {
-        private readonly HttpClient _httpClient;
+        private readonly IAddressDAO _addressDAO;
 
-        public AddressService(IHttpClientFactory factory) {
-            _httpClient = factory.CreateClient("GHN");
+        public AddressService(IAddressDAO addressDAO) {
+            _addressDAO = addressDAO;
+        }
+
+        public void Create(Address address) {
+            _addressDAO.Create(address);
+        }
+
+        public void Delete(int addressId) {
+            
+        }
+
+        public Address Get(int addressId) {
+            return _addressDAO.Get(addressId);
+        }
+
+        public List<Address> GetByCustomerId(int customerId) {
+            return _addressDAO.GetByCustomerId(customerId).ToList();
+        }
+
+        public void Update(Address address) {
+            var db = _addressDAO.Get(address.Id);
+
+            if (db == null) {
+                throw new Exception("404: Address not found");
+            }
+
+            db.RecipientName = address.RecipientName;
+            db.RecipientPhone = address.RecipientPhone;
+            db.Province = address.Province;
+            db.District = address.District;
+            db.DistrictId = address.DistrictId;
+            db.Ward = address.Ward;
+            db.WardCode = address.WardCode;
+            db.Street = address.Street;
+            db.UpdatedAt = DateTime.Now;
+
+            _addressDAO.Update(db);
         }
     }
 }
