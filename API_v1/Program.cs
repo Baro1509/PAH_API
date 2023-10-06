530578393ec236eb9c612c0de92d99b8da523ba6
hoang bao <baohoang.7882@hotmail.com>
2023-10-06 13:44:16 +0700

    update: add authorization to swagger

--- API_v1/Program.cs
+++ API_v1/Program.cs
@@ -38,16 +38,14 @@ builder.Services.AddSwaggerGen(c => {
         In = ParameterLocation.Header,
         Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
     });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
-            {
-                new OpenApiSecurityScheme {
-                    Reference = new OpenApiReference {
-                        Type = ReferenceType.SecurityScheme,
-                            Id = "Bearer"
-                    }
-                },
-                new string[] {}
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {{
+        new OpenApiSecurityScheme {
+            Reference = new OpenApiReference {
+                Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
             }
+        },
+        new string[] {}}
     });
 });
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
