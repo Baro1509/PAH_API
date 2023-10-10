e73e6597acd6c9a4ce3aaa98fad91029853c09d2
hoang bao <baohoang.7882@hotmail.com>
2023-10-10 18:39:50 +0700

    feat: add api to test

---
+++ API_v1/Controllers/WalletController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Service;
+
+namespace API.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WalletController : ControllerBase {
+        private readonly IWalletService _walletService;
+
+        public WalletController(IWalletService walletService) {
+            _walletService = walletService;
+        }
+
+        [HttpGet]
+        public IActionResult Get() {
+            _walletService.Topup(1, new Service.ThirdParty.Zalopay.OrderRequest());
+            return Ok();
+        }
+    }
+}
