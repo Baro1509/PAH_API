700bad79586c421e4e97b44915c6429c02f98afa
hoang bao <baohoang.7882@hotmail.com>
2023-10-10 18:40:43 +0700

    feat: add third-party models

---
+++ Service/ThirdParty/Zalopay/OrderRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.ThirdParty.Zalopay {
+    public class OrderRequest {
+        public decimal Topup { get; set; }
+        public string AppId { get; set; }
+        public string AppTransId { get; set; }
+        public string Mac { get; set; }
+    }
+}
