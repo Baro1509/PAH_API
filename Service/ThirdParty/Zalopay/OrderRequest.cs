f934f86d1966134bc9453af53034c2d4bad79268
hoang bao <baohoang.7882@hotmail.com>
2023-10-11 16:21:42 +0700

    update: change datatype

--- Service/ThirdParty/Zalopay/OrderRequest.cs
+++ Service/ThirdParty/Zalopay/OrderRequest.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace Service.ThirdParty.Zalopay {
     public class OrderRequest {
         public decimal Topup { get; set; }
-        public string AppId { get; set; }
+        public int AppId { get; set; }
         public string AppTransId { get; set; }
         public string Mac { get; set; }
     }
