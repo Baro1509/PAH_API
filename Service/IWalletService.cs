73b79129925a29dfe2f814cde27152124b66d825
hoang bao <baohoang.7882@hotmail.com>
2023-10-11 16:22:02 +0700

    feat: implement checkout wallet

--- Service/IWalletService.cs
+++ Service/IWalletService.cs
@@ -8,7 +8,8 @@ using System.Threading.Tasks;
 namespace Service {
     public interface IWalletService {
         public void Topup(int userId, OrderRequest orderRequest);
-        public void SubtractBalance(int userId, decimal balance);
+        public void CheckoutWallet(int userId, int orderId);
+        public void CheckoutZalopay(int userId, int orderId, OrderRequest orderRequest);
         public void AddLockedBalance(int userId, decimal balance);
         public void SubtractLockedBalance(int userId, decimal balance);
     }
--- Service/Implement/WalletService.cs
+++ Service/Implement/WalletService.cs
@@ -11,6 +11,7 @@ namespace Service.Implement {
     public class WalletService : IWalletService {
         private readonly IWalletDAO _walletDAO;
         private readonly ITransactionDAO _transactionDAO;
+        private readonly IOrderDAO _orderDAO;
         private readonly IHttpClientFactory _httpClientFactory;
 
         public WalletService(IWalletDAO walletDAO, ITransactionDAO transactionDAO, IHttpClientFactory httpClientFactory) {
@@ -25,15 +26,25 @@ namespace Service.Implement {
                 throw new Exception("404: Wallet not found");
             }
 
+            //Check transaction from zalopay in committed in db
+            if (!_transactionDAO.IsZalopayOrderValid(orderRequest.AppTransId, orderRequest.Mac)) {
+                throw new Exception("409: Order from zalopay is invalid");
+            }
+
+            //Call to zalopay
             var httpClient = _httpClientFactory.CreateClient("Zalopay");
-            var data = new QueryRequest();
-            data.SetDataInProcess1();
+            var data = new QueryRequest {
+                app_id = orderRequest.AppId,
+                app_trans_id = orderRequest.AppTransId,
+                mac = orderRequest.Mac
+            };
             var httpResponseMessage = await httpClient.PostAsync("query", Utils.ConvertForPost<QueryRequest>(data));
-            
+
             if (!httpResponseMessage.IsSuccessStatusCode) {
                 throw new Exception("400: No order in zalo pay yet");
             }
 
+            //Validation
             var responseData = await httpResponseMessage.Content.ReadAsAsync<QueryResponse>();
             if (responseData.return_code != 1) {
                 throw new Exception("400: " + responseData.return_message);
@@ -42,25 +53,63 @@ namespace Service.Implement {
                 throw new Exception("400: Amount does not match with order from zalopay");
             }
 
+            //Topup and create transaction
             wallet.AvailableBalance += orderRequest.Topup;
             _walletDAO.Update(wallet);
             _transactionDAO.Create(new DataAccess.Models.Transaction {
                 Id = 0,
                 WalletId = wallet.Id,
-                PaymentMethod = 1
+                PaymentMethod = (int) PaymentType.Zalopay,
+                Amount = orderRequest.Topup,
+                Type = (int) TransactionType.Deposit,
+                Date = DateTime.Now,
+                Description = $"app_id: {orderRequest.AppId}, " +
+                    $"app_trans_id: {orderRequest.AppTransId}, " +
+                    $"mac: {orderRequest.Mac}",
+                Status = (int) Status.Available
             });
-            
         }
 
         public void AddLockedBalance(int userId, decimal balance) {
             throw new NotImplementedException();
         }
 
-        public void SubtractBalance(int userId, decimal balance) {
+        public void SubtractLockedBalance(int userId, decimal balance) {
             throw new NotImplementedException();
         }
 
-        public void SubtractLockedBalance(int userId, decimal balance) {
+        public void CheckoutWallet(int userId, int orderId) {
+            var wallet = _walletDAO.Get(userId);
+            if (wallet == null) {
+                throw new Exception("404: Wallet not found");
+            }
+
+            var order = _orderDAO.Get(orderId);
+            if (order == null) {
+                throw new Exception("404: Order not found when checkout");
+            }
+
+            if (wallet.AvailableBalance < order.TotalAmount) {
+                throw new Exception($"400: Not enough balance in wallet to confirm order: {orderId}");
+            }
+
+            //Subtract from wallet, create transaction, update order
+            wallet.AvailableBalance -= order.TotalAmount;
+            _walletDAO.Update(wallet);
+            _transactionDAO.Create(new DataAccess.Models.Transaction {
+                Id = 0,
+                WalletId = wallet.Id,
+                PaymentMethod = (int) PaymentType.Wallet,
+                Amount = order.TotalAmount,
+                Type = (int) TransactionType.Payment,
+                Date = DateTime.Now,
+                Description = $"Payment for order: {orderId}",
+                Status = (int) Status.Available
+            });
+            //Change order status here
+        }
+
+        public void CheckoutZalopay(int userId, int orderId, OrderRequest orderRequest) {
             throw new NotImplementedException();
         }
     }
