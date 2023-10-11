496cf85522809819c38c39e1194bdddc6f6ef9f2
hoang bao <baohoang.7882@hotmail.com>
2023-10-11 20:44:54 +0700

    feat: add topup api

--- API_v1/Controllers/WalletController.cs
+++ API_v1/Controllers/WalletController.cs
@@ -1,7 +1,12 @@
-﻿using Microsoft.AspNetCore.Cors;
+﻿using API.ErrorHandling;
+using API.Response;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Service;
+using Service.Implement;
+using Service.ThirdParty.Zalopay;
+using System.Net;
 
 namespace API.Controllers {
     [Route("api/[controller]")]
@@ -9,15 +14,42 @@ namespace API.Controllers {
     [EnableCors]
     public class WalletController : ControllerBase {
         private readonly IWalletService _walletService;
+        private readonly IUserService _userService;
 
-        public WalletController(IWalletService walletService) {
+        public WalletController(IWalletService walletService, IUserService userService) {
             _walletService = walletService;
+            _userService = userService;
         }
 
-        [HttpGet]
-        public IActionResult Get() {
-            _walletService.Topup(1, new Service.ThirdParty.Zalopay.OrderRequest());
-            return Ok();
+        private int GetUserIdFromToken() {
+            var user = HttpContext.User;
+            return int.Parse(user.Claims.FirstOrDefault(p => p.Type == "UserId").Value);
+        }
+
+        [HttpPost("topup")]
+        public async Task<IActionResult> Topup([FromBody] OrderRequest request) {
+            var userId = GetUserIdFromToken();
+            if (userId == null) {
+                return Unauthorized(new ErrorDetails { 
+                    StatusCode = (int) HttpStatusCode.Unauthorized, 
+                    Message = "You are not logged in" 
+                });
+            }
+
+            var user = _userService.Get(userId);
+            if (user == null) {
+                return Unauthorized(new ErrorDetails { 
+                    StatusCode = (int) HttpStatusCode.Unauthorized, 
+                    Message = "You are not allowed to access this" 
+                });
+            }
+
+            await _walletService.Topup(userId, request);
+            return Ok(new BaseResponse { 
+                Code = (int) HttpStatusCode.OK, 
+                Message = "Topup successfully", 
+                Data = null 
+            });
         }
     }
 }
