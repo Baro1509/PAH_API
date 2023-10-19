4e9ddbaa321f81c01d678de21d5841da4dca369d
hoang bao <baohoang.7882@hotmail.com>
2023-10-19 15:19:27 +0700

    feat: add base admin

---
+++ API_v1/Controllers/AdminController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class AdminController : ControllerBase {
+    }
+}
