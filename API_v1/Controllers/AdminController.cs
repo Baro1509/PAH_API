f068916a681ee98da664004818aa05bdbdb07001
hoang bao <baohoang.7882@hotmail.com>
2023-10-20 10:31:11 +0700

    feat: implement create and update staff

--- API_v1/Controllers/AdminController.cs
+++ API_v1/Controllers/AdminController.cs
@@ -1,20 +1,47 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using API.ErrorHandling;
+using AutoMapper;
+using DataAccess.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Request;
+using Respon;
+using Service;
+using System.Net;
 
 namespace API.Controllers {
     [Route("api/[controller]")]
     [ApiController]
     [Authorize]
     public class AdminController : ControllerBase {
+        private readonly IMapper _mapper;
+        private readonly IAdminService _adminService;
+
+        public AdminController(IMapper mapper, IAdminService adminService) {
+            _mapper = mapper;
+            _adminService = adminService;
+        }
+
         [HttpPost("staff")]
-        public IActionResult AddStaff() {
-            return Ok();
+        [ServiceFilter(typeof(ValidateModelAttribute))]
+        public IActionResult AddStaff([FromBody] StaffRequest request) {
+            _adminService.CreateStaff(_mapper.Map<User>(request));
+            return Ok(new BaseResponse {
+                Code = (int) HttpStatusCode.OK,
+                Message = "Create staff successfully",
+                Data = null
+            });
         }
         
         [HttpPatch("staff")]
-        public IActionResult EditStaff() {
-            return Ok();
+        [ServiceFilter(typeof(ValidateModelAttribute))]
+        public IActionResult EditStaff([FromBody] StaffRequest request) {
+            _adminService.UpdateStaff(_mapper.Map<User>(request));
+            return Ok(new BaseResponse {
+                Code = (int) HttpStatusCode.OK,
+                Message = "Create staff successfully",
+                Data = null
+            });
         }
 
         [HttpGet("account")]
