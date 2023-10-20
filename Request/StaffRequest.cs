03293e9487ec7e56170eaeff922d74432ebdb5ba
hoang bao <baohoang.7882@hotmail.com>
2023-10-20 10:31:37 +0700

    feat: add staff request

---
+++ Request/StaffRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Request {
+    public class StaffRequest {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string? Phone { get; set; }
+        public string? ProfilePicture { get; set; }
+        [Required]
+        public int? Gender { get; set; }
+        [Required]
+        public DateTime? Dob { get; set; }
+        [Required]
+        public int Status { get; set; }
+    }
+}
