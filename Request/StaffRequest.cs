f43da3a612d8602cc094ebca725407c60f6438b5
hoang bao <baohoang.7882@hotmail.com>
2023-10-20 14:41:22 +0700

    update: add annotation

--- Request/StaffRequest.cs
+++ Request/StaffRequest.cs
@@ -10,8 +10,10 @@ namespace Request {
         [Required]
         public string Name { get; set; }
         [Required]
+        [EmailAddress]
         public string Email { get; set; }
         [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
         [Required]
         public string? Phone { get; set; }
